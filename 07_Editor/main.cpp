0f91dae730a2d1a76990e085d7b4c5dde1859857
Sávila Saraiva <37075994+savilafs@users.noreply.github.com>
2018-05-16 09:37:59 -0300

    Add files via upload

---
+++ 07_Editor/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <list>
+#include <sstream>
+#include <SFML/Graphics.hpp>
+#include "sftext.h"
+
+using namespace std;
+
+struct Ambiente{
+    list<char> texto;
+    list<char>::iterator cursor;
+    Ambiente(){
+        cursor = texto.end();
+    }
+
+    string toString(){
+        stringstream ss;
+        for(auto it = texto.begin(); it != texto.end(); it++){
+            if(it == cursor)
+                ss << "#";
+            ss << (*it);
+        }
+        if(cursor == texto.end())
+            ss << "#";
+        return ss.str();
+    }
+};
+
+int main(){
+    Ambiente amb;
+
+    sf::RenderWindow janela(sf::VideoMode(800, 600), "Janela");
+
+    while(janela.isOpen()){
+
+        sf::Event event;
+        while(janela.pollEvent(event)){
+            if(event.type == sf::Event::Closed)
+                janela.close();
+            if(event.type == sf::Event::KeyPressed){
+                if((event.key.code >= sf::Keyboard::A) &&
+                   (event.key.code <= sf::Keyboard::Z)){
+                    char tecla = 'a' + (event.key.code - sf::Keyboard::A);
+                    cout << tecla << endl;
+                    amb.texto.insert(amb.cursor, tecla);
+                }
+                else if(event.key.code == sf::Keyboard::Z){
+                    if(event.key.control)
+                        cout << "control z" << endl;
+                }
+                else if(event.key.code == sf::Keyboard::R){
+                    if(event.key.control)
+                        cout << "control r" << endl;
+                }
+                else if((event.key.code >= sf::Keyboard::A) &&
+                   (event.key.code <= sf::Keyboard::Z)){
+                    char tecla = (event.key.code - sf::Keyboard::A) + 'a';
+                    cout << tecla << endl;
+                }
+                else if((event.key.code == sf::Keyboard::Space)){
+                    cout << "espaco" << endl;
+                    amb.texto.insert(amb.cursor, ' ');
+                }
+                else if(event.key.code == sf::Keyboard::BackSpace){
+                    cout << "backspace" << endl;
+                    //amb.texto.remove();
+                    if(amb.cursor != amb.texto.begin())
+                         amb.texto.erase(amb.cursor++,amb.cursor--);
+
+                }else if(event.key.code == sf::Keyboard::Delete){
+                    cout << "delete" << endl;
+                    //amb.texto.pop_front();
+                    if(amb.cursor != amb.texto.end())
+                        amb.texto.erase(amb.cursor++);
+                }
+                else if(event.key.code == sf::Keyboard::Left){
+                    amb.cursor--;
+                }
+                else if(event.key.code == sf::Keyboard::Right){
+                    amb.cursor++;
+                }
+            }
+        }
+
+        auto pos = sf::Vector2f(30, 50);
+        janela.clear();
+        janela.draw(sfText(pos, amb.toString(), sf::Color::White, 30));
+        janela.display();
+    }
+    return 0;
+}
